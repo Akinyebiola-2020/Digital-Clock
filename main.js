3a6db525a05ce389ba4e72a0b640c931cd3e774a
Akinyebiola-2020 <122544121+Akinyebiola-2020@users.noreply.github.com>
2023-05-31 10:37:50 +0100

    Update main.js
    This is an updated main.js

--- main.js
+++ main.js
@@ -4,48 +4,38 @@ setInterval (second, 1000);
 	 		let today = new Date();
 	 		let s = today.getSeconds();					
 	 		 if (s < 10)
-       { document.getElementById("seconds").innerHTML= "0" + s + " s";
+       {document.getElementById("seconds").innerHTML= "0" + s + " s";
+			 }
 
-		}
-
-	else { document.getElementById("seconds").innerHTML = s + " s";
-				 
-		}
+	else {document.getElementById("seconds").innerHTML = s + " s";
+	      }
 									 	
-		}
-
-
 setInterval (minute)
  function minute() {
 	let today = new Date();	
 	let m = today.getMinutes();
-     if (m < 10)
-     { document.getElementById("minutes").innerHTML = "0" + m + " m";
-
-    }
-        else {	document.getElementById("minutes").innerHTML = m + " m";
-
-	      	}
-
-	    	}
-
-
+   if (m < 10)
+   {document.getElementById("minutes").innerHTML = "0" + m + " m";
+   }
+ else {document.getElementById("minutes").innerHTML = m + " m";
+      }
+		}
 		setInterval (hour)
 		function hour() {
 	 	let today = new Date();
 		let h = today.getHours();
       
-	 if ( h > 12 )
-     {	document.getElementById("hours").innerHTML = "0" + (h-12) + " PM";
-
-	 }
-
-       else if ( h = 12)
-      {  document.getElementById("hours").innerHTML = h + " PM"
-
+	 	 if ( h >= 1 && h < 10 )
+     {	document.getElementById("hours").innerHTML = "0" + h + " AM";
+	 		 }
+      else if (h <= 11 ){
+        document.getElementById("hours").innerHTML = h + " AM";
+      }
+      else if (h = 12 ){
+        document.getElementById("hours").innerHTML = h + " PM";
+      }
+      else if ( h > 12)
+      {    document.getElementById("hours").innerHTML = (h-12) + " PM";
       }
       
-	else { document.getElementById("hours").innerHTML = "0" + h + " AM";
-
-	}
-	} 
+				} 
